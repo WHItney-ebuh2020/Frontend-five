11098028308e186859f4b5092f1dcb83323040fd
Whitney Ebuh <whitneyebuh2020@gmail.com>
2023-08-19 17:41:12 -1100

    color commit

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const colors = ["blue", "red", "yellow", "#0000FF"];
 const btn = document.getElementById("btn");
 const color = document.querySelector("color");
 
-btn.addEventListener("click", function() { 
-    const getNumber = getAllNumbers();
-    console.log(getNumber);
+btn.addEventListener("click", function() {
+const getNumber = getAllNumbers();
+console.log(getNumber);
 
     document.body.style.backgroundColor = colors[getNumber];
-    color.textContent = colors[randomNumber];
+
 })
 
 function getAllNumbers() {
-    return Math.random() * colors.length;
+return Math.floor(Math.random() * colors.length);
 }
